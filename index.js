95160277f25dad274f1867854a9139aa9ee37de0
Juan Agustín Zitelli <juanzitelli7@gmail.com>
2020-10-17 01:25:02 -0300

    Added .env files

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-
+require("dotenv").config();
 
 app.get('/', (req, res) => {  
 	res.json('{message: Hello Heroku! This should be deployed}')
 })
-app.listen(4000, ()=> {
+
+
+app.listen(process.env.PORT, ()=> {
+	console.log(process.env);
 	console.log("server running on port 4000")
 })
